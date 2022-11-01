a6672de046818e0530d74aefa0f8cd3ec4fd3707
Your Name <you@example.com>
2022-11-01 15:14:44 +0400

    thre

--- pages/index.jsx
+++ pages/index.jsx
@@ -42,56 +42,6 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <div>
-          <meta charSet="UTF-8" />
-          <meta name="description" content="My Portfolio" />
-          <meta name="keywords" content="NextJs, React, JavaScript" />
-          <meta name="author" content="Erik Harutyunyan" />
-          <meta property="og:locale" content="en_US" />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="Portfolio" />
-          <meta
-            property="og:description"
-            content="Portfolio Erik Harutyunyan"
-          />
-          <meta property="og:image" content="/img/common/ogp_sun.jpg" />
-          <meta property="og:site_name" content="Portfolio Erik Harutyunyan" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Portfolio" />
-          <meta
-            name="twitter:description"
-            content="Portfolio Erik Harutyunyan"
-          />
-          <meta name="twitter:image" content="/img/common/ogp_sun.jpg" />
-          <title>Portfolio</title>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#7b7b7b" />
-          <meta name="msapplication-TileColor" content="#220600" />
-          <meta name="theme-color" content="#220600" />
-        </div>
-      </Head>
       <main>
         {isLoader && <DynamicLoader />}
         <DynamicCursor />
